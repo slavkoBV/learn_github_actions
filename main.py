7eeda38aec041875b7deb0e703a0a30b07c38ca0
Yaroslav Bezghachniuk <slav_b@ukr.net>
2025-10-15 10:19:55 +0300

    add main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+
+def exchange_coins(cash: int) -> dict:
+    coins = {}
+    for i in (50, 25, 10, 5, 2, 1):
+        res = cash // i
+        if res:
+            coins[i] = res
+        cash %= i
+    return coins
+
+
+if __name__ == '__main__':
+    initial_cash = 1000
+    result = exchange_coins(initial_cash)
+    print(result)
